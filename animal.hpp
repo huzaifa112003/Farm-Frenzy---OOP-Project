049db8bd029e128bb0b3b573963ba7801bc0cfc9
huzaifa112003 <98843738+huzaifa112003@users.noreply.github.com>
2023-04-14 04:06:17 +0500

    animal class

---
+++ animal.hpp
@@ -0,0 +1,11 @@
+#include<SDL.h>
+#pragma once
+
+class Animal{
+    SDL_Renderer* gRenderer;
+    SDL_Texture* assets;
+public:
+    Animal(SDL_Renderer*, SDL_Texture*);
+    virtual void draw(SDL_Rect srcRect, SDL_Rect moverRect) = 0;
+    virtual void sound() = 0;
+};
