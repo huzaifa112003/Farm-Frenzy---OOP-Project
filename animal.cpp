ff27b9dc5280902df723e826fa681713692cac49
maleeka320 <89735446+maleeka320@users.noreply.github.com>
2023-05-01 18:24:46 +0500

    Delete animal.cpp

--- animal.cpp
+++
@@ -1,9 +0,0 @@
-#include"animal.hpp"
-
-Animal::Animal(SDL_Renderer* rend, SDL_Texture* ast): gRenderer(rend), assets(ast){
-
-}
-
-void Animal::draw(SDL_Rect srcRect, SDL_Rect moverRect){
-    SDL_RenderCopy(gRenderer, assets, &srcRect, &moverRect);
-}
