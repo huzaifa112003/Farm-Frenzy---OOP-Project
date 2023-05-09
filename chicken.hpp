237f366c0a74879b018f574ec9af61ab95c5c62c
maleeka320 <mf07423@st.habib.edu.pk>
2023-05-09 14:29:37 +0500

    all classes

---
+++ chicken.hpp
@@ -0,0 +1,21 @@
+#include<SDL.h>
+#include "animal.hpp"
+#include "egg.hpp"
+#include "food.hpp"
+#include <windows.h>
+// sheep implementation will go here.
+#include <time.h>
+class chicken: public Animal{
+SDL_Rect srcRect, moverRect;
+
+
+public:
+    food* anda;
+    void draw(SDL_Renderer*, SDL_Texture* assets); //draw function of sheep
+    void move(); //move function
+    bool flag = true; //flag helps in making condition for sheep
+    int animation = 0; // controls animation
+    //sheep(); //default constructor
+    chicken(SDL_Renderer*, SDL_Texture*, int, int); // may add other overloaded constructors here...  
+    food* createProduct(SDL_Renderer* rend, SDL_Texture* texture);
+};
