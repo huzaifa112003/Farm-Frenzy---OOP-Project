e499aec6550bd9df3c1cb6bdd05ce661ec45d5ca
maleeka320 <mf07423@st.habib.edu.pk>
2023-05-09 17:24:21 +0500

    updated src rects of products

--- meat.cpp
+++ meat.cpp
@@ -8,7 +8,8 @@ void meat::draw(SDL_Renderer* gRenderer, SDL_Texture* assets){
 
 meat::meat(SDL_Renderer* rend, SDL_Texture* texture, int x, int y):
   food(rend, texture) { // overloaded constructor
-    srcRect = {557, 1457, 49, 39};
+    // srcRect = {557, 1457, 49, 39};
+    srcRect={131,1545,128,75};
     //srcRect = {0, 2, 218, 212};
     moverRect = {x, y, 30, 40}; // egg drawn at x,y coordinates
 }
